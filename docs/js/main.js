8489526b6ce492b8b6e83497ceceb4c88d1126a7
Sardor Kalon <kalonsr@gmail.com>
2020-05-17 22:29:40 +0500

    Next commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,17 +3,24 @@ $(document).ready(function () {
     const mobileMenuToggle = document.querySelector('.toggle-menu');
     const mobMenu = document.querySelector('.header-nav-dropdown');
     const overlay = document.querySelector('#overlay');
+    const primer = document.querySelector('.primer');
+    
+    
 
     mobileMenuToggle.addEventListener('click', function () {
         mobMenu.classList.toggle('active');
         this.classList.toggle('active');
         overlay.classList.toggle('active');
+        primer.classList.toggle('active');
+        
     });
 
     window.addEventListener('resize', function () {
         mobMenu.classList.remove('active');
         mobileMenuToggle.classList.remove('active');
         overlay.classList.remove('active');
+        primer.classList.remove('active');
+        
     })
 
 })
