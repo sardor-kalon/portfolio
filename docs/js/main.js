3e904ec7278e5c07575463d379107b4218f4181f
Sardor Kalon <kalonsr@gmail.com>
2020-05-23 04:37:23 +0500

    Contact section and Footer added

--- docs/js/main.js
+++ docs/js/main.js
@@ -55,3 +55,25 @@ $(document).ready(function () {
 
 
 })
+
+$(document).ready(function () { 
+
+ const formInputs = document.querySelectorAll(".form-field");
+ 
+ for (let item of formInputs) {
+
+    const inputPlaceholder = item.nextElementSibling;
+
+    item.addEventListener("click", function () {
+        inputPlaceholder.classList.add("active");
+    })
+
+    item.addEventListener("blur", function () { 
+        if(this.value == ""){
+            inputPlaceholder.classList.remove("active");
+        }
+    })
+
+ }
+
+})
