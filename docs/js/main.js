cdae58034e390e89b6a3f9d5c7b1444f9ba22ec5
Sardor Kalon <kalonsr@gmail.com>
2020-05-29 22:19:05 +0500

    Page nav added

--- docs/js/main.js
+++ docs/js/main.js
@@ -136,3 +136,23 @@ $(window).scroll( function () {
         $("#backTop").fadeOut();
     }
 });
+
+// Page Nav
+
+$(document).ready(function () {
+
+    $("#page-nav").onePageNav({
+        currentClass: "active",
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        filter: "",
+        easing: "swing",
+        begin: function () {},
+        end: function () {},
+        scrollChange: function ($currentListItem) {}
+    });
+
+    
+
+})
